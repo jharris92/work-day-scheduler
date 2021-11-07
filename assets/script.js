70b0a7caf62e72c6667b5763d6c3a3ba1927e049
John Harris <john.harris7292@gmail.com>
2021-11-07 17:02:43 -0500

    bug fix

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,50 @@
 var saveBtn = $(".saveBtn");
 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
-
-function timeBlockColor() {
-    var hour = moment().hours();
-
-    $(".time-block").each(function() {
-        var currHour = parseInt($(this).attr("id"));
-
-        if (currHour > hour) {
-            $(this).addClass("future");
-        } else if (currHour === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("past");
-        }
-        
-    })
-};
-
-saveBtn.on("click", function(){
-    var time = $(this).sibilings(".hour").text();
-    var plan = $(this).sibilings(".plan").val();
-
-    localStorage.setItem(time,plan);
-});
-
-function usePlanner() {
-    $(".hour").each(function(){
-        var currHour = $(this).text();
-        var currPlan = localStorage.getItem(currHour);
-
-        if(currPlan !== null) {
-            $(this).sibilings(".plan").val(currPlan);
-        }
-    });
-}
-
-timeBlockColor();
-usePlanner();
+ 
+ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+ 
+ // each time block is color-coded to indicate whether it is in the past, present, or future
+ function timeBlockColor() {
+     var hour = moment().hours();
+ 
+     $(".time-block").each(function() {
+         var currHour = parseInt($(this).attr("id"));
+ 
+ 
+         if (currHour > hour) {
+             $(this).addClass("future");
+         } else if (currHour === hour) {
+             $(this).addClass("present");
+         } else {
+             $(this).addClass("past");
+         }
+     })
+ };
+ 
+ // WHEN I click the save button for that time block
+ saveBtn.on("click", function() {
+ 
+     // console.log(this); //save button
+     var time = $(this).siblings(".hour").text();
+     var plan = $(this).siblings(".plan").val();
+ 
+     // THEN the text for that event is saved in local storage
+     localStorage.setItem(time, plan);
+ });
+ 
+ // WHEN I refresh the page
+ // THEN the saved events persist
+ function usePlanner() {
+ 
+     $(".hour").each(function() {
+         var currHour = $(this).text();
+         var currPlan = localStorage.getItem(currHour);
+ 
+         if(currPlan !== null) {
+             $(this).siblings(".plan").val(currPlan);
+         }
+     });
+ }
+ 
+ timeBlockColor();
+ usePlanner();
